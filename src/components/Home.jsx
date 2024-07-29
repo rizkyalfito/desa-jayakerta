c991437ad051f4bb2a4d479a8f57656469ce7491
Rizky Alfito <ikycollege@gmail.com>
2024-07-30 01:45:36 +0700

    fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,20 +6,43 @@ import PotensiDesa from './potensiDesa';
 import Location from './Location';
 import About from './About';
 import Jumbotron from './Jumbotron';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+import { useEffect } from 'react';
+
 
 const Home = () => {
   const bgColor = useColorModeValue('gray.50', 'gray.800');
+
+  useEffect(() => {
+    AOS.init({
+      duration: 1200, // Durasi animasi
+    });
+  }, []);
 
   return (
     <>
       <Hero />
       <div className="container mx-auto p-4" style={{ backgroundColor: bgColor }}>
-        <About/>
-        <News />
-        <Jumbotron/>
-        <Gallery />
-        <PotensiDesa />
-        <Location/>
+        <div data-aos="fade-up">
+          <About/>
+        </div>
+        <div data-aos="fade-up">
+          <News />
+        </div>
+        <div data-aos="fade-up">
+          <Jumbotron/>
+        </div>
+        <div data-aos="fade-up">
+          <Gallery />
+        </div>
+        <div data-aos="fade-up">
+          <PotensiDesa />
+        </div>
+        <div data-aos="fade-up">
+          <Location/>
+        </div>
       </div>
     </>
   );
